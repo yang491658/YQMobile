4e82410321b37309ecaaf5ba0356b83cb238e8a1
yang491658 <park49164357@gmail.com>
2025-12-12 19:11:13 +0900

    코드 점검

--- Assets/Scripts/Managers/TestManager.cs
+++ Assets/Scripts/Managers/TestManager.cs
@@ -160,7 +160,7 @@ public class TestManager : MonoBehaviour
 
             totalScore += score;
             maxScore = Mathf.Max(score, maxScore);
-            averageScore = ++testCount> 0 ? totalScore / testCount : 0;
+            averageScore = totalScore / ++testCount;
 
             UpdateTestUI();
 
@@ -226,7 +226,7 @@ public class TestManager : MonoBehaviour
 
         _config.slider.value = _config.value;
     }
-    private void ChangeGameSpeed(float _value) => ApplySlider(ref gameSpeed, _value, v => GameManager.Instance.SetSpeed(v, true));
+    private void ChangeGameSpeed(float _value) => ApplySlider(ref gameSpeed, _value, v => GameManager.Instance?.SetSpeed(v, true));
 
     private void UpdateTestUI()
     {
